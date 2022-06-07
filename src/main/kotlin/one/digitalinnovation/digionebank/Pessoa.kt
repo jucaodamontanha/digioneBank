7ea1e53b2c6904ef6663bbead77a2ecbb08bbd8f
LIneker Xavier <62408720+jucaodamontanha@users.noreply.github.com>
2022-06-07 10:40:17 -0300

    update to aplly the privite modifier to set function of cpf propriedades

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,10 +4,13 @@ class Pessoa {
     var nome: String = "Lineker"
     var cpf: String = "123.456.789-12"
     private set
+
+    constructor()
+    fun pessoaInfo() = "$nome  e  $cpf"
 }
 
-fun main(){
+fun main() {
     val lineker = Pessoa()
-    println(lineker.nome)
-    println(lineker.cpf)
+    print(lineker.pessoaInfo())
+
 }
